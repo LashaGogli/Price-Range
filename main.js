bfc8276f40bf45e77cb08f30f2378142e7295d54
Lasha <lashagoglidzelasha@gmail.com>
2023-02-20 15:55:28 +0400

    Changed

--- main.js
+++ main.js
@@ -4,8 +4,47 @@ let lowToHigh = document.querySelector("#lowToHigh");
 let HighToLow = document.querySelector("#HighToLow");
 let aToZ = document.querySelector("#aToZ");
 let ZtoA = document.querySelector("#ZtoA");
+let Min = document.querySelector("#Min");
+let Max = document.querySelector("#Max");
+let filter = document.querySelector("#filter");
 
 
+
+
+filter.addEventListener("click", function () {
+    container.innerHTML = '';
+    let filtered = products.filter((n) => n[1] >= Min.value && n[1] <= Max.value);
+    filtered.forEach((fruit) => {
+
+        let box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+
+        let name = document.createElement("p");
+        box.appendChild(name);
+        name.innerHTML = "Product Name: " + fruit[0];
+
+        let price = document.createElement("p");
+        box.appendChild(price);
+        price.innerHTML = `${"Product Price: " + fruit[1]}$`;
+    });
+})
+
+
+products.forEach((fruit) => {
+
+    let box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+
+    let name = document.createElement("p");
+    box.appendChild(name);
+    name.innerHTML = "Product Name: " + fruit[0];
+
+    let price = document.createElement("p");
+    box.appendChild(price);
+    price.innerHTML = `${"Product Price: " + fruit[1]}$`;
+});
 
 lowToHigh.addEventListener("click", function () {
     container.innerHTML = '';
